f3bf1d492f8f24f66d474bc6046448145bf274fd
mmavrich <mmavrich@gmail.com>
2020-11-21 20:59:05 -0500

    retest

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -7,12 +7,12 @@ from pathlib import Path
 def load_module(directory,name):
     sys.path.insert(0,directory)
     importlib.import_module(name)
-    print ("Importing lib ",name)
+    #print ("Importing lib ",name)
     sys.path.pop(0)
 
 def load_directory(directory):
     for path in directory.rglob('*.py'):
-        load_module(path.as_posix(),path.stem)
+        load_module(directory.as_posix(),path.stem)
 
 def load_bundled():
     directory=Path(__file__).parent / "extensions"
